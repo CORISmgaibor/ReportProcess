94932fba630df58a6fbfd306c8f71a0caf97ef33
Miguel Angel Gaibor Mariño <mgaibor@coris.ec>
2022-12-19 15:47:31 -0500

    feat: ejecuta SP y genera reporte excel

---
+++ ConvertUtility.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using ClosedXML.Excel;
+namespace ReportProcess
+{
+    public static class ConvertUtility
+    {
+        public static void ToCSV(this System.Data.DataTable dtDataTable, string strFilePath)
+
+
+        {
+            StreamWriter sw = new StreamWriter(strFilePath, false);
+            //headers    
+            for (int i = 0; i < dtDataTable.Columns.Count; i++)
+            {
+                sw.Write(dtDataTable.Columns[i]);
+                if (i < dtDataTable.Columns.Count - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+            foreach (DataRow dr in dtDataTable.Rows)
+            {
+                for (int i = 0; i < dtDataTable.Columns.Count; i++)
+                {
+                    if (!Convert.IsDBNull(dr[i]))
+                    {
+                        string value = dr[i].ToString();
+                        if (value.Contains(','))
+                        {
+                            value = String.Format("\"{0}\"", value);
+                            sw.Write(value);
+                        }
+                        else
+                        {
+                            sw.Write(dr[i].ToString());
+                        }
+                    }
+                    if (i < dtDataTable.Columns.Count - 1)
+                    {
+                        sw.Write(",");
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
+        }
+
+
+        public static void ToExcel(this System.Data.DataTable dtDataTable, string strFilePath)
+        {
+            XLWorkbook wb = new XLWorkbook();
+            //var imagePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources\coris.png");
+            var pagina = wb.Worksheets.Add(dtDataTable, "WorksheetName");
+            pagina.Row(1).InsertRowsAbove(5);
+            pagina.Range("A1:C3");
+            //pagina.AddPicture(imagePath).MoveTo(pagina.Cell("A1")).Scale(1.5);
+            wb.SaveAs(strFilePath);
+
+        }
+
+    }
+}
--- Reporter.cs
+++ Reporter.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
-using System.IO;
-using System.Reflection;
-
+using System.Data;
 
 namespace ReportProcess
 {
@@ -31,12 +29,50 @@ namespace ReportProcess
         private static void run()
         {
             string strConn = properties.get("Process.Connection", "");
+            string descripcion = "", nombreQuery = "", cadena = "", extension = "", repoSalida = "";
             SqlConnection conn = new SqlConnection(strConn);
-            SqlCommand cmd = new SqlCommand("", conn);
+            //SqlCommand cmd = new SqlCommand(@"SELECT A.DESCRIPCION , B.NOMBRE , D.CADENA , F.EXTENSION 
+            //                                  FROM CAMPANA A JOIN QUERY B ON A.IDCAMPANA = B.IDCAMPANA
+            //                                  JOIN CONEXIONES_QUERY C ON C.IDQUERY=B.IDQUERY 
+            //                                  JOIN CONEXIONES D ON D.IDCONEXIONES = C.IDCONEXIONES
+            //                                  JOIN FORMATO_QUERY E ON E.IDQUERY = B.IDQUERY
+            //                                  JOIN FORMATOS F ON E.IDFORMATO = F.IDFORMATO
+            //                                  WHERE A.ESTADO = 1 AND B.ESTADO = 1", conn);
+            SqlCommand cmd = new SqlCommand(@"SELECT 'CAMPAÑA DE PRUEBA' AS DESCRIPCION , 'SP_TEST' AS NOMBRE , 'Data Source=192.168.0.185;Initial Catalog=Reportiador;Persist Security Info=True;User ID=sa;Password=Administrator1;TrustServerCertificate=True' AS CADENA, 'XLSX' AS EXTENSION, '' AS PARAMETROS , '\\192.168.10.4\Compartida temp\Reporteador\' as URLSALIDA", conn);
 
             try
             {
                 conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    descripcion = (string)reader["DESCRIPCION"];
+                    nombreQuery = (string)reader["NOMBRE"];
+                    cadena = (string)reader["CADENA"];
+                    extension = (string)reader["EXTENSION"];
+                    repoSalida = (string)reader["URLSALIDA"];
+
+                    SqlConnection tempConn = new SqlConnection(cadena);
+                    SqlCommand tempCmd = new SqlCommand(nombreQuery, tempConn);
+                    tempConn.Open();
+                    try
+                    {
+                        tempCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        SqlDataAdapter da = new SqlDataAdapter(tempCmd);
+                        DataTable table = new DataTable();
+                        da.Fill(table);
+                        //table.ToCSV(repoSalida + descripcion + ".csv"  );
+                        table.ToExcel(repoSalida + descripcion + "." + extension);
+
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                    finally
+                    {
+                        tempConn.Close();
+                    }
+                }
 
             }
             catch (Exception e)
